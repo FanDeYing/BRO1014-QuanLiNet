6986efeb1446c721c9b3dba8e348cd96974edd6f
Fan DeYing <chickenblue648@gmail.com>
2025-07-20 14:07:36 +0700

    Update code 20/7/2025

--- PRO1041-QLNet/src/net/ui/manager/QuanLyTaiKhoanJDialog.java
+++ PRO1041-QLNet/src/net/ui/manager/QuanLyTaiKhoanJDialog.java
@@ -33,11 +33,11 @@ public class QuanLyTaiKhoanJDialog extends javax.swing.JDialog {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 661, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 438, Short.MAX_VALUE)
         );
 
         pack();
--- PRO1041-QLNet/src/net/utils/ketNoiDB.java
+++ PRO1041-QLNet/src/net/utils/ketNoiDB.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class ketNoiDB {
     public static Connection getConnectDB() throws SQLException{
-        String urlSQL = "jdbc:sqlserver://localhost:1433;databaseName=PTPM_SOF2042_FINAL;"
+        String urlSQL = "jdbc:sqlserver://localhost:1433;databaseName=QuanLiQuanNet;"
                 + "user=sa;password=1234;encrypt=true;"
                 + "trustServerCertificate=true";
         Connection con = DriverManager.getConnection(urlSQL);
